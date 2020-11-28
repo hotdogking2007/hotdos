6c934450113554b892be84d055d7d5096dbeb45c
hotdogking2007 <73842491+hotdogking2007@users.noreply.github.com>
2020-11-28 13:38:05 +0900

    hotdos 0.2v
    버그를 끝네고 0.2버전을 출시했습니당
    ---명령어---
    htds name - 이름을 출력함
    htds see (file name) - 텍스트 파일의 내용을 출력함

---
+++ hotdos.0.2.py
@@ -0,0 +1,25 @@
+print("made in hotdog union version(0.2)")
+hotdos_date = open("hotdos_date/name.txt", "r")
+hotdos_name = hotdos_date.readline()
+hotdos_date.close()
+print(hotdos_name, "실행 성공")
+while True:
+    if hotdos_name == "no date":
+        print("당신의 이름은 무엇입니까?")
+        console = input(">>>")
+        hotdos_date = open("hotdos_date/name.txt", "w")
+        hotdos_date.write(console)
+        hotdos_name = hotdos_date
+        hotdos_date.close()
+    console = input("---명령어---\nhtds name-내이름\nhtds see (file(first))\nfile - 파일 보기\n>>>")
+    if console == "htds name":
+        print(hotdos_name)
+    if console == "htds see first":
+        hotdos_date = open("hotdos_date/first.txt", "r", encoding='UTF-8')
+        while 1 :
+            line = hotdos_date.readline()
+            if not line:
+                break
+            
+            print(line)
+        
